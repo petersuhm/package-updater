77107db3aa443cdb66316ac256ac84d1a9cb8cd1
Peter Suhm <peter@suhm.dk>
2015-01-14 08:51:57 +0000

    initial class

---
+++ WpPusher/Updater/PackageUpdater.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace WpPusher\Updater;
+
+class PackageUpdater
+{
+    public function config(array $options)
+    {
+        // ...
+    }
+
+    public function enable()
+    {
+        // ...
+    }
+}
