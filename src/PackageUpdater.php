2b9b531e8d7164a50c81db7f05815049f998ff30
Peter Suhm <peter@suhm.dk>
2015-01-14 09:20:38 +0000

    init updater class

--- src/PackageUpdater.php
+++ src/PackageUpdater.php
@@ -2,15 +2,34 @@
 
 namespace WpPusher\Updater;
 
+use WpPusher\Updater\Auth\AuthProvider;
+
 class PackageUpdater
 {
+    protected $zipballUrl;
+    protected $authProvider;
+
     public function config(array $options)
     {
-        // ...
+        $this->setZipballUrl($options['zipball_url']);
+        $this->setAuthProvider($options['auth_provider']);
     }
 
     public function enable()
     {
-        // ...
+        if ($authProvider)
+            $authProvider->register();
+
+        // Do updater stuff
+    }
+
+    public function setAuthProvider(AuthProvider $provider)
+    {
+        $this->authProvider = $provider;
+    }
+
+    public function setZipballUrl($url)
+    {
+        $this->zipballUrl = $url;
     }
 }
